c0701a5e8631ff58b37fc03ba09c92bd96780914
ollavrova <krocozabr@gmail.com>
2013-03-20 00:28:33 +0200

    add jquery+ajax

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here
+
 class Books(models.Model):
     name = models.CharField(max_length=100, verbose_name="Name")
     description = models.CharField(max_length=100, verbose_name="Description")
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from annoying.decorators import render_to
 from django.http import Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from books.models import Books
 from django.template import RequestContext
 
@@ -8,14 +8,20 @@ from django.template import RequestContext
 @render_to('main.html')
 def main(request):
     items = Books.objects.all()
-    return {'items' : items}
+    return {'items': items}
 
-def detail(request,id):
+
+def detail(request, id):
     try:
         id = int(id)
         item = Books.objects.filter(pk=id)[0]
     except ValueError:
         raise Http404()
-    return render_to_response('detail.html', {'item' : item, 'id' : id,},
-                                context_instance=RequestContext(request))
-
+    if request.is_ajax():
+        import time
+        time.sleep(5)
+        # response is just the form
+        return render(request, 'add.html', {'item': item, 'id': id}, context_instance=RequestContext(request))
+    else:
+        # response is the entire page
+        return render(request, 'detail.html', {'item': item, 'id': id}, context_instance=RequestContext(request))
