6ee82a763f037cb40bd61a02dea8d5abd7b1f788
ollavrova <krocozabr@gmail.com>
2013-05-04 09:47:31 +0300

    fix bugs

--- listOfBooks/settings.py
+++ listOfBooks/settings.py
@@ -72,6 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_PATH + 'books/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -111,7 +112,7 @@ ROOT_URLCONF = 'listOfBooks.urls'
 WSGI_APPLICATION = 'listOfBooks.wsgi.application'
 
 TEMPLATE_DIRS = (
-    PROJECT_PATH +"/books/templates",
+    PROJECT_PATH +"books/templates/",
     )
 
 INSTALLED_APPS = (
